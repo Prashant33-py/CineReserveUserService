689753d0d6980130dde7fb5c5c5e55c0ad11e0f0
Prashant33-py <pvasala786@gmail.com>
2025-11-24 20:59:14 +0530

    Removes the @EnableMethodSecurity annotation in user service

--- src/main/java/com/cinereserve/app/CineReserveUserService/config/CineSecurityConfig.java
+++ src/main/java/com/cinereserve/app/CineReserveUserService/config/CineSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,7 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
 public class CineSecurityConfig {
 
     @Autowired
